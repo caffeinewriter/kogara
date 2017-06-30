dfb5f7fd8b89bc62391f28609b0bc488b203d0a6
Brandon Anzaldi <banzaldi@fapl.co>
2017-06-29 20:47:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Kogara
+
+## A simple, ugly, Go/Redis-backed URL shortener.
+
+### Naming
+
+Kogara (小柄) is the result of extensive Google Translating variations of the word "short" into different languages, because that's how I like to name my projects. Plus, Kogara sounds pretty damn cool.
+
+### Development Status
+
+This is a hobby project designed to increase my own familiarity with Go, Redis, and Gin. I'm striving to have it become a stable, full-fledged project, but keep that in mind if you decide to use it. 
+
+### Routes
+
+`/` is a simple homepage to shorten links.
+
+`/+/:id` displays a simple counter of the times the link has loaded from the server. (It uses a 301 redirect, so browsers will typically aggressively cache the result)
+
+`/r/:id` is the redirect itself. 
+
+`/check/:id` will return a JSON blob revealing the existence of a particular ID. Mainly implemented so if I decide to add custom URLs.
+
+### Current caveats
+
+* At present solely generates sequential base62 link IDs.
+* At present has no way to administer links beyond directly managing them in Redis.
+* Does not confirm link existence, or even structure, so you may end up with some weird results.
+* May be vulnerable to XSS, but most browsers reject execution of JavaScript unless it's directly entered by the user into the address bar. 
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+  "fmt"
+  
+  "github.com/gin-gonic/gin"
+  "github.com/go-redis/redis"
+  "github.com/pilu/go-base62"
+)
+
+func main() {
+  r := gin.Default()
+  
+  red := redis.NewClient(&redis.Options{
+    Addr:     "localhost:6379",
+    Password: "",
+    DB:       0,
+  })
+  
+  _, err := red.Ping().Result()
+  
+  if err != nil {
+    panic(err)
+  }
+  
+  r.GET("/", func (ctx *gin.Context) {
+    ctx.Header("Content-Type", "text/html")
+    ctx.String(200, `<!DOCTYPE html>
+                     <html>
+                      <head>
+                        <title>Kogara - Link Shortening</title>
+                      </head>
+                      <body>
+                        <form action="/" method="POST">
+                          <input type="text" name="url"><br>
+                          <input type="submit" value="Shorten">
+                        </form>
+                      </body>
+                      </html>`);
+  })
+  
+  r.POST("/", func (ctx *gin.Context) {
+    if ctx.PostForm("url") == "" {
+      ctx.String(400, "URL cannot be empty.")
+    }
+    count, err := red.Incr("kogara:counter").Result()
+    if err != nil {
+      ctx.String(500, "Unable to create link.")
+    }
+    id := base62.Encode(int(count));
+    _, err = red.Set("kogara:links:" + id, ctx.PostForm("url"), 0).Result()
+    if err != nil {
+      ctx.String(500, "Unable to create link.")
+    } else {
+      ctx.Header("Content-Type", "text/html")
+      ctx.String(200,fmt.Sprintf(`<!DOCTYPE html>
+                      <html>
+                      <head>
+                        <title>Kogara - Link Created</title>
+                      </head>
+                      <body>
+                        Created <a href="/r/%s">/r/%s</a>
+                      </body>
+                      </html>`, id, id))
+    }
+  })
+  
+  r.GET("/r/:id", func (ctx *gin.Context) {
+    link, err := red.Get("kogara:links:" + ctx.Params.ByName("id")).Result()
+    if err == redis.Nil {
+      ctx.String(404, "This link does not seem to exist.")
+    } else if err != nil {
+      ctx.String(500, "Sorry, something went wrong while trying to fetch this link.")
+    } else {
+      red.Incr("kogara:analytics:" + ctx.Params.ByName("id"))
+      ctx.Redirect(301, link)
+    }
+  })
+  
+  r.GET("/+/:id", func (ctx *gin.Context) {
+    count, err := red.Get("kogara:analytics:" + ctx.Params.ByName("id")).Result()
+    if err == redis.Nil {
+      ctx.String(404, "Not found")
+    } else if err != nil {
+      ctx.String(500, "Something went wrong.")
+    } else {
+      ctx.String(200, count)
+    }
+  })
+  
+  r.GET("/check/:id", func (ctx *gin.Context) {
+    exists, err := red.Exists("kogara:links:" + ctx.Params.ByName("id")).Result()
+    if err != nil {
+      ctx.JSON(500, gin.H{"error": true, "available": nil})
+    } else {
+      ctx.JSON(200, gin.H{"error": nil, "available": exists != 1})
+    }
+  })
+  
+  r.Run(":8081")
+}
